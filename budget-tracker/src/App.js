e9cc613879139cbf07f7fc4849e4bca4cc55f796
Amanda Lin <amandalincs@gmail.com>
2020-12-15 13:00:33 -0800

    Updated dependencies and added reactstrap

--- budget-tracker/src/App.js
+++ budget-tracker/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { Button, Container } from 'reactstrap'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: 20 }}>
+      <p className="text-primary">You clicked {count} times.</p>
+      <Button onClick={() => setCount(count + 1)} color="success">
+        Increase the count
+      </Button> <Button onClick={() => setCount(count - 1)} color="danger">
+        Decrease the count
+      </Button>
+    </Container>
   );
 }
 
