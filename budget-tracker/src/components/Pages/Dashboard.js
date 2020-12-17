a66565de6af6b3e50f54afbae5a6fd8d72e04cf0
Kevin Huynh <kevinhuynh@Kevins-MacBook-Pro.local>
2020-12-16 23:40:13 -0800

    able to remove row item

--- budget-tracker/src/components/Pages/Dashboard.js
+++ budget-tracker/src/components/Pages/Dashboard.js
@@ -30,6 +30,7 @@ const Styles = styled.div`
 function Dashboard() {
 
     const [items, setItems] = useState([])
+    const [itemToDelete, setItemToDelete] = useState()
     const [deleteModal, setDeleteModal] = useState(false);
     // const handleDeleteAllItems = () => {
     //     setItems([])
@@ -42,12 +43,24 @@ function Dashboard() {
 
     const handleDeleteAllItems = () => {
         setItems([])
-        localStorage.clear()
+        localStorage.clear()        
+    }
+
+    const handleItemToDelete = (item) => {
+        console.log(item)
+    }
+    const handleDeleteItem = (event) => {
+        let id = event.currentTarget.value
+        let new_items = items.filter( (expense) => {
+            return expense.id !== id
+        })
+        localStorage.setItem('items', JSON.stringify(new_items))
+        setItems(new_items)
+
     }
 
     useEffect(() => {
         let ls = JSON.parse(localStorage.getItem('items'))
-        console.log(ls)
         if (ls) setItems(ls)
     },[])
 
@@ -74,7 +87,11 @@ function Dashboard() {
                             </ModalFooter>
                         </Modal>
                     </div>
-                    <ItemList items={items}></ItemList>
+
+                    {items.length > 0 ?
+                    (<ItemList items={items} handleItemToDelete={handleItemToDelete} handleDeleteItem={handleDeleteItem}></ItemList>) 
+                    : (<div style={{textAlign: 'center', padding: '20px 10px'}}>No Data Available</div>)
+                    }
                 </Card>
 
             </Container>
